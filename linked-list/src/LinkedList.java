f90b38a1421eb2ef2fc10f804864ec906167776a
Paul Yakovlev <yvpavel@gmail.com>
2019-02-22 22:03:27 -0800

    begin writing LinkedList class

--- linked-list/src/LinkedList.java
+++ linked-list/src/LinkedList.java
@@ -1,2 +1,39 @@
+import java.util.List;
+
 public class LinkedList {
+    //properties
+    Node head;
+    int count;
+
+    //constructors
+    public LinkedList() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    public LinkedList(Node head){
+        this.head = head;
+        this.count = 1;
+    }
+
+    //methods
+    public void addNode(){
+
+    }
+
+    public void removeNode(){
+
+    }
+
+    public int getCount(){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public int get(int nodeNum){
+
+    }
 }
