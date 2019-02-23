b84cb9811d0d25ca5c3d9f3a3eef57052910db9d
Paul Yakovlev <yvpavel@gmail.com>
2019-02-22 21:55:01 -0800

    write Node module

---
+++ linked-list/src/Node.java
@@ -0,0 +1,30 @@
+public class Node {
+    int value;
+    Node next;
+
+    public Node(int value){
+        this.value = value;
+        next = null;
+    }
+
+    public Node(int value, Node next){
+        this.value = value;
+        next = next;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public Node getNext(){
+        return this.next;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+    }
+    
+    public void setNext(Node next){
+        this.next = next;
+    }
+}
